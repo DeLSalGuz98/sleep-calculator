0c239201d7a7238871cc9fbf39753743c7fac75f
Denilson Salas G <denilsonsalguz@gmail.com>
2023-11-16 20:09:42 -0500

    add form inputs and buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import './App.css'
 
 import { Navbar } from './components/navbar'
+import { FormCalculator } from './components/calculator'
 
 function App() {
+  
   return (
     <>
       <Navbar/>
       <div className='banner'>
         <div className='container w-100'>
-          <div className="card border-light mb-3">
-            <div className="card-body">
-              <form>
-                <input className="form-control me-sm-2" type="search" placeholder="Search"/>
-                <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-              </form>
-            </div>
-          </div>
+          <FormCalculator/>
         </div>
       </div>    
     </>
