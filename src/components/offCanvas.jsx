4a01d51059ad6a7745f56cfe12923d610692a2eb
Denilson Salas G <denilsonsalguz@gmail.com>
2023-11-22 16:26:52 -0500

    Update Readme file and add screenshots

--- src/components/offCanvas.jsx
+++ src/components/offCanvas.jsx
@@ -4,7 +4,7 @@ export function OffCanvas() {
   return(
     <div className="offcanvas offcanvas-start" tabIndex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
       <div className="offcanvas-header">
-        <h5 className="offcanvas-title" id="offcanvasExampleLabel">Más informacion</h5>
+        <h5 className="offcanvas-title" id="offcanvasExampleLabel">Más información</h5>
         <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div className="offcanvas-body">
